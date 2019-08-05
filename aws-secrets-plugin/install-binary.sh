300a856a992bd8aebbca6398f96c81abf4c30b44
Xingzhong Min <xingzhong.min@modopayments.com>
2019-08-05 15:56:00 -0500

    modified install-helm.sh

--- aws-secrets-plugin/install-binary.sh
+++ aws-secrets-plugin/install-binary.sh
@@ -9,11 +9,11 @@ case "${HELM_BIN}" in
         HELM_DIR="$(dirname ${HELM_BIN})"
         ;;
 esac
-if [[ `command -v kubectl-aws_secrets help` ]]; then
+#if [[ `command -v kubectl-aws_secrets help` ]]; then
   echo "kubectl-aws_secrets already exists"
-else
+#else
   echo "installing kubectl-aws_secrets"
   curl -sS -o kubectl-aws-secrets.tar.gz https://github.com/xmin-github/kubectl-aws-secrets/blob/master/bin/kubectl-aws-secrets.tar.gz
   sudo tar -xzvf kubectl-aws-secrets.tar.gz
   sudo cp kubectl-aws_secrets /usr/local/bin
-fi
+#fi
